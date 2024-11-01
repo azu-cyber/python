a9187deb4ecee309117fa141936b682eb2949d45
azu-cyber <avikalizhimomi@gmail.com>
2024-11-01 18:26:55 +0530

    O(n)Time

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ def printnumber(n):
 printnumber (10) 
 printnumber (20)
 print("\n With any 'n' the time taken by our code won't change")
+def OnTime(n):
+    iteration=0
+    for i in range(1,n+1):
+        iteration+=1
+    print ("When n is ",n," Iterations = ",iteration)
+OnTime (10)
+OnTime (20)
+OnTime (42)
+print("\nWith every 'n' the time taken and iterations will increase")

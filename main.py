42eeb1ac99f7b77c638d98180232781545ed1483
azu-cyber <avikalizhimomi@gmail.com>
2024-11-01 18:41:36 +0530

    O(n)square

--- main.py
+++ main.py
@@ -15,3 +15,16 @@ OnTime (10)
 OnTime (20)
 OnTime (42)
 print("\nWith every 'n' the time taken and iterations will increase")
+def ONSquareTime(n):
+    iteration=0
+    for i in range(0,n):
+        for j in range(0, n):
+            print ("*", end =" ")
+            iteration+=1
+        print("")
+    print ("\nWhen n is ",n," Iterations = ",iteration, "\n")
+ONSquareTime(5)
+ONSquareTime(4)
+ONSquareTime(3)
+print("\nWith every 'n' the time taken equals n^2")
+print ("0(n^2)")

1d399bc53b436200c6a98a41de693c89cfaec848
azu-cyber <avikalizhimomi@gmail.com>
2024-11-06 17:39:35 +0530

    space

--- space time complexity/main.py
+++ space time complexity/main.py
@@ -4,4 +4,13 @@ def prints(n):
     print ("Codingal")
     prints(n/2)
     prints(n/2)
-prints(5)
+prints(4)
+def sum(n):
+    return n*(n+1)/2
+def arraysum(a):
+    sum=0
+    for i in a:
+        sum = sum + i
+    return (sum)
+a = [12, 3, 4, 15]
+arraysum(a)
